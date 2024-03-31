e8a5abfec7141da91bf86d1646970e8edcc7f76a
David <zerbib.david.1@gmail.com>
2024-03-31 21:21:20 +0200

    wip search results

--- app/[lng]/searchresults/page.tsx
+++ app/[lng]/searchresults/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 import React from "react";
 import SearchRoomReservation from "@/app/components/ui/SearchRoomReservation";
+import Navbar from "@/app/components/NavBar";
+import HomeBody from "@/app/components/HomeBody";
 
 export default function Page() {
     return (
-        <div>
-            <SearchRoomReservation/>
+        <div className="">
+            <div className="relative w-full">
+                <div className="w-full ">
+                    <Navbar/>
+                </div>
+                <div className="mt-32 w-full">
+                    <SearchRoomReservation/>
+
+                </div>
+            </div>
+
+            <div style={{height: '130vh'}}>
+            </div>
         </div>
-
-
     )
 }
