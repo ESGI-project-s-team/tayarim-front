47330de2b9dde4744669a3d56f957ddbb1553038
QuentinCoqueran <coqueranquentin@gmail.com>
2024-04-08 12:08:36 +0200

    update for build to fix

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import HomeBody from "@/app/components/HomeBody";
-import NavbarScroll from "@/app/components/NavbarScroll";
-import Loader from "@/app/components/Loader";
 import React from "react";
 
 export default function Page() {
