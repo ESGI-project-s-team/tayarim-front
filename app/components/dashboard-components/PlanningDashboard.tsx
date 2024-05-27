4228826c69eb2585c0e73f6c5a871cbc0aca47a0
David <zerbib.david.1@gmail.com>
2024-05-27 20:41:45 +0200

    planning

--- app/components/dashboard-components/PlanningDashboard.tsx
+++ app/components/dashboard-components/PlanningDashboard.tsx
@@ -1,56 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import {checkTokenInFun} from "@/app/components/ui/signin/action";
 import {useRouter} from "next/navigation";
-import {useNavbarContext, useTranslationContext} from "@/app/[lng]/hooks";
 import 'react-datepicker/dist/react-datepicker.css';
-import {isBefore, startOfWeek} from "date-fns";
-type Shift = {
-    employee: string;
-    start: string;
-    end: string;
-    role: string;
-    color: string;
-};
-
-const shifts: Shift[] = [
-    { employee: 'Diana Jackson', start: '06:00', end: '11:00', role: 'CASHIER', color: 'bg-blue-500' },
-    { employee: 'Blake Shields', start: '06:00', end: '11:00', role: 'BARISTA', color: 'bg-purple-500' },
-    { employee: 'Isabel Perez', start: '09:00', end: '14:00', role: 'CASHIER', color: 'bg-blue-500' },
-    { employee: 'Ahmed (AJ) Ayad', start: '09:00', end: '14:00', role: 'BARISTA', color: 'bg-purple-500' },
-    { employee: 'Finn Halonen', start: '09:00', end: '13:00', role: 'TRAINING', color: 'bg-orange-500' },
-    { employee: 'June Lee', start: '11:00', end: '13:00', role: 'TEAM LEAD', color: 'bg-green-500' },
-    { employee: 'June Lee', start: '13:00', end: '19:00', role: 'TEAM LEAD', color: 'bg-red-500' },
-    { employee: 'Bruce Garrison', start: '14:00', end: '19:00', role: 'CASHIER', color: 'bg-blue-500' }
-];
-
-const generateMonthDays = (date: Date) => {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const firstDayOfMonth = new Date(year, month, 1);
-    const lastDayOfMonth = new Date(year, month + 1, 0);
-    const daysInMonth = [];
-
-    // Current month days
-    for (let day = 1; day <= lastDayOfMonth.getDate(); day++) {
-        daysInMonth.push(new Date(year, month, day));
-    }
-
-    // Days from next month to fill up to 35 days
-    const nextMonthDaysNeeded = 35 - daysInMonth.length;
-    for (let i = 1; i <= nextMonthDaysNeeded; i++) {
-        daysInMonth.push(new Date(year, month + 1, i));
-    }
-
-    return daysInMonth;
-};
+import {addDays, format, startOfWeek, subDays} from 'date-fns';
+import {fr} from 'date-fns/locale';  // Importer le locale français si nécessaire
 
 const PlanningDashboard: React.FC = () => {
     const router = useRouter();
-    const {translation} = useTranslationContext();
-    const [isLoading, setIsLoading] = useState(false);
-    const [filterButtonState, setFilterButtonState] = useState(0);
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const days = generateMonthDays(selectedDate);
+    const [currentWeekStart, setCurrentWeekStart] = useState<Date>(startOfWeek(new Date(), {weekStartsOn: 1}));
+
+    const nextWeek = () => {
+        setCurrentWeekStart(addDays(currentWeekStart, 7));
+    };
+
+    const previousWeek = () => {
+        setCurrentWeekStart(subDays(currentWeekStart, 7));
+    };
+
+    const goToToday = () => {
+        setCurrentWeekStart(startOfWeek(new Date(), {weekStartsOn: 1}));
+    };
+
+    const days = Array.from({length: 7}).map((_, index) => addDays(currentWeekStart, index));
+    const people = [
+        "Diana Jackson",
+        "Blake Shields",
+        "Isabel Perez",
+        "Ahmed (AJ) Ayad",
+        "Finn Halonen",
+        "June Lee",
+        "Bruce Garrison"
+    ];
+    const schedules = [
+        [" ", "#3b82f6", "#3b82f6", "", "", "", ""], // Diana Jackson
+        [" ", "#8b5cf6", "#8b5cf6", "#8b5cf6", "#8b5cf6", "#8b5cf6", ""], // Blake Shields
+        ["#f59e0b", "#f59e0b", "#f59e0b", "", "", "", ""], // Isabel Perez
+        ["#10b981", "#10b981", "#10b981", "#10b981", "#10b981", "#10b981", "#10b981"], // Ahmed (AJ) Ayad
+        ["#ef4444", "#ef4444", "#ef4444", "", "", "", ""], // Finn Halonen
+        ["#6366f1", "", "", "", "", "", ""], // June Lee
+        ["#3b82f6", "#3b82f6", "#3b82f6", "#3b82f6", "#3b82f6", "#3b82f6", ""], // Bruce Garrison
+    ];
 
     useEffect(() => {
         checkTokenInFun().then((response) => {
@@ -60,51 +49,105 @@ const PlanningDashboard: React.FC = () => {
         });
     }, [router]);
 
+    const getMergedSchedules = (schedule: string[]) => {
+        const merged = [];
+        let currentColor = schedule[0];
+        let span = 1;
+
+        for (let i = 1; i < schedule.length; i++) {
+            if (schedule[i] === currentColor) {
+                span++;
+            } else {
+                if (currentColor) {
+                    merged.push({color: currentColor, span});
+                }
+                currentColor = schedule[i];
+                span = 1;
+            }
+        }
+        if (currentColor) {
+            merged.push({color: currentColor, span}); // Add the last segment
+        }
+
+        return merged;
+    };
+
     return (
-        <div className="lg:ml-80 mr-2 ml-14 z-0">
-            <div className="h-screen relative top-32 w-full">
-                <WeekView
-                    days={days}
-                    title={`Week of ${selectedDate.toLocaleDateString()}`}
-                    onNext={() => setSelectedDate(new Date(selectedDate.setMonth(selectedDate.getMonth() + 1)))}
-                    onPrev={() => setSelectedDate(new Date(selectedDate.setMonth(selectedDate.getMonth() - 1)))}
-                    onToday={() => setSelectedDate(new Date())}
-                    showTodayButton={true}
-                    HeaderComponent={HeaderComponent}
-                />
+        <div className="h-screen lg:ml-80 lg:mr-7 mr-2 ml-14 z-0 overflow-auto bg-gray-100">
+            <div className="relative top-20 w-full mb-2">
+                <div className="container mx-auto mt-8">
+                    <div className="flex justify-around mb-4 items-center space-x-2 bg-white p-4 shadow rounded">
+                        <button onClick={previousWeek} className="text-[#3c50e0] hover:underline px-2 py-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                 stroke="currentColor" className="w-7 h-7">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                        </button>
+                        <div className="text-center">
+                            <h2 className="text-base sm:text-lg md:text-xl font-medium">
+                                {format(currentWeekStart, 'MMMM yyyy', {locale: fr})}
+                            </h2>
+                            <button onClick={goToToday} className="text-[#3c50e0] underline text-base">
+                                Today
+                            </button>
+                        </div>
+                        <button onClick={nextWeek} className="text-[#3c50e0] hover:underline px-2 py-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                 stroke="currentColor" className="w-7 h-7">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="overflow-x-auto">
+                        <div className="inline-block min-w-full">
+                            <div className="grid grid-cols-10 gap-0 bg-white shadow rounded">
+                                <div className="col-span-2 flex flex-col border-r border-gray-300">
+                                    <div className="h-14 border-b border-gray-300"></div>
+                                    {people.map((person, index) => (
+                                        <div key={index}
+                                             className="border-b border-gray-300 p-2 flex-1 flex items-center justify-center">
+                                            <h4 className="text-xs sm:text-xs md:text-sm lg:text-sm font-normal truncate">{person}</h4>
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="col-span-8">
+                                    <div className="grid grid-cols-7 gap-0">
+                                        {days.map((day, dayIndex) => (
+                                            <div key={dayIndex}
+                                                 className="border-r border-b border-gray-300 p-2 h-14 flex items-center justify-center">
+                                                <h3 className="text-xs sm:text-xs md:text-sm lg:text-sm font-medium truncate">{format(day, 'EEE dd', {locale: fr})}</h3>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    {people.map((person, personIndex) => (
+                                        <div key={personIndex} className="grid grid-cols-7 gap-0">
+                                            {getMergedSchedules(schedules[personIndex]).map((block, blockIndex, array) => (
+                                                <div
+                                                    key={blockIndex}
+                                                    className={`border-r border-gray-300 border-b  p-2 flex items-center justify-center`}
+                                                    style={{gridColumn: `span ${block.span}`}}
+                                                >
+                                                    <div className="h-12 w-full"
+                                                         style={{backgroundColor: block.color}}></div>
+                                                </div>
+
+                                            ))}
+                                            {Array.from({length: 7 - getMergedSchedules(schedules[personIndex]).reduce((acc, block) => acc + block.span, 0)}).map((_, i) => (
+                                                <div key={i}
+                                                     className="border-r border-b border-gray-300 p-2 flex items-center justify-center"></div>
+                                            ))}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
-const HeaderComponent = ({title, onNext, onPrev, onToday, showTodayButton}) => (
-    <div className="flex items-center justify-between p-4 bg-gray-200">
-        <button onClick={onPrev} className="btn">Previous</button>
-        <h1 className="text-lg font-bold">{title}</h1>
-        <button onClick={onNext} className="btn">Next</button>
-        {showTodayButton && <button onClick={onToday} className="btn">Today</button>}
-    </div>
-);
-
 export default PlanningDashboard;
-
-export function WeekView({days, title, onNext, onPrev, onToday, showTodayButton, HeaderComponent}) {
-    return (
-        <div>
-            {HeaderComponent && <HeaderComponent
-                title={title}
-                onNext={onNext}
-                onPrev={onPrev}
-                onToday={onToday}
-                showTodayButton={showTodayButton}
-            />}
-            <div className="grid grid-cols-7 gap-4 p-4">
-                {days.map((day, index) => (
-                    <div key={index} className="border p-4">
-                        <h2 className="text-lg">{day.toLocaleDateString()}</h2>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
