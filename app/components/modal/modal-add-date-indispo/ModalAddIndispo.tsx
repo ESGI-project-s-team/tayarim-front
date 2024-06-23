7057dea707ca28b21632d1f5858905a7dc8de417
David <zerbib.david.1@gmail.com>
2024-06-23 20:26:59 +0200

    delete indispo

--- app/components/modal/modal-add-date-indispo/ModalAddIndispo.tsx
+++ app/components/modal/modal-add-date-indispo/ModalAddIndispo.tsx
@@ -135,38 +135,43 @@ export default function ModalAddIndispo({isOpen, onClose, id, getAllIndispo, dat
                                             <div className="p-7">
                                                 <h5 className={"mb-2 text-sm font-medium text-gray-950 mt-2"}>{translation?.t('add_indispo')}</h5>
                                                 <div>
-                                                    {datesBloquer?.length > 0 ?
+                                                    {datesBloquer?.length > 0 ? (
                                                         <div
-                                                            className="flex flex-wrap gap-2 text-sm  max-h-32 overflow-y-auto no-scrollbar ">
+                                                            className="flex flex-wrap gap-2 text-sm max-h-32 overflow-y-auto no-scrollbar p-2 bg-gray-50 rounded-lg ">
                                                             {datesBloquer.map((date: any, index: number) => (
-                                                                <div key={index}
-                                                                     className={"flex max-h-32 overflow-y-auto  cursor-pointer "}
-                                                                     onClick={() => {
-                                                                         handleDeleteIndispo(date.id)
-                                                                     }}
+                                                                <div
+                                                                    key={index}
+                                                                    className="flex items-center bg-white p-2 rounded-md shadow-sm border cursor-pointer hover:bg-red-50 transition duration-300 ease-in-out"
+                                                                    onClick={() => handleDeleteIndispo(date.id)}
                                                                 >
                                                                     <DateFormaterEnFr date={date.dateDebut}
                                                                                       theLanguage={theLanguage}/>
-                                                                    <span> - </span>
+                                                                    <span className="mx-1">-</span>
                                                                     <DateFormaterEnFr date={date.dateFin}
                                                                                       theLanguage={theLanguage}/>
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                         viewBox="0 0 24 24"
-                                                                         strokeWidth="1.5" stroke="currentColor"
-                                                                         className="w-4 h-4 ml-3 mt-0.5 text-red-600">
-                                                                        <path strokeLinecap="round"
-                                                                              strokeLinejoin="round"
-                                                                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
+                                                                    <svg
+                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                        fill="none"
+                                                                        viewBox="0 0 24 24"
+                                                                        strokeWidth="1.5"
+                                                                        stroke="currentColor"
+                                                                        className="w-4 h-4 ml-3 text-red-600"
+                                                                    >
+                                                                        <path
+                                                                            strokeLinecap="round"
+                                                                            strokeLinejoin="round"
+                                                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                                                        />
                                                                     </svg>
                                                                 </div>
                                                             ))}
                                                         </div>
-                                                        :
-                                                        <div className="mt-2 text-xs">
-                                                            {translation?.t('no_indispo')}
-                                                        </div>
-                                                    }
+                                                    ) : (
+                                                        <div
+                                                            className="mt-2 text-xs text-gray-500">{translation?.t('no_indispo')}</div>
+                                                    )}
                                                 </div>
+
                                                 <h5 className={"mb-2 text-sm font-medium text-gray-950 mt-10"}>{translation?.t('indispo_dates')}</h5>
                                                 <DatePickerRangerCustomForm
                                                     datesIndispo={datesIndispo}
