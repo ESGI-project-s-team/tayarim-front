e7b4f3b1b67017499737383becc5834f909872af
David <zerbib.david.1@gmail.com>
2024-06-26 16:10:31 +0200

    request owner WIP

--- app/components/home-components/OwnerConnection.tsx
+++ app/components/home-components/OwnerConnection.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import FormConnection from "@/app/components/ui/signin/FormConnection";
 import BackgroundImageDark from "@/app/components/ui/BackgroundImageDark";
+import ModalCreateOwner from "@/app/components/modal/modal-create-owner/ModalCreateOwner";
 
 const OwnerConnection: React.FC = () => {
     return (
-        <div className="overflow-hidden">
-            <BackgroundImageDark/>
-            <FormConnection/>
+        <div className="overflow-hidden ">
+            <div
+                className=" absolute w-full h-screen  bg-image flex lg:justify-around justify-center items-center ">
+                <div className={"z-30"}>
+                    <FormConnection/>
+                </div>
+                <div className={"bg-white min-w-1 min-h-[70vh] hidden lg:block top-36 absolute z-30"}>
+                </div>
+                <div className={"hidden lg:block z-30"}>
+                    <FormConnection/>
+                </div>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-70"></div>
         </div>
     );
 }
