3aade9d90dbc535e5548df2d5d3cfd98fa92a936
David <zerbib.david.1@gmail.com>
2024-07-06 15:22:24 +0200

    some fix

--- app/[lng]/search-results/details/[id]/page.tsx
+++ app/[lng]/search-results/details/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import NavBarSearch from "@/app/components/search-results-components/ui/NavBarSearch";
 import DetailsResult from "@/app/components/details-result/DetailsResult";
 import NavBarDetails from "@/app/components/search-results-components/ui/NavBarDetails";
 
